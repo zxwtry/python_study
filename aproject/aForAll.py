0a11128fd59544b95c6fdff44de4b95f55f70cf9
zxwtry <zxwtry@qq.com>
2016-05-11 10:01:40 +0800

    add aForAll.py

---
+++ aproject/aForAll.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+a = ("a", "b", "c")
+s = {1:111, 5:555, 4:444, 3:333, 2:222}
+for k,v in s.items():
+    print str(k)+" "+str(v)
+for x in range( len(a) ):
+    print a[x]
+for x in s:
+    print x
