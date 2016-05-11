b7890839c02d954704ee845186e3a7ac96da8654
zxwtry <zxwtry@qq.com>
2016-05-11 21:21:33 +0800

    modify aFunction01.py

--- aproject/aFunction01.py
+++ aproject/aFunction01.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python
+#coding:utf8
+#coding=utf8
+#coding:utf8
+#encoding=utf8
+#_*_ coding:utf8 _*_
+
+
+def fun04(x=1,y=1):
+    print "create ",x, "yuan", y,"ge", "icecream"
+
+
+fun04(y="KKK")
+fun04()
+fun04(1,4)
+def fun03(x):
+    print x
+
+s=raw_input(":");
+fun03(s)
+
+
+def fun02():
+    if True:
+        print "OK"
+
+fun02()
 
 def fun01():
     if True:
