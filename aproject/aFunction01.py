705716bd743b9e22a9776e301a9caa4747ed8cfe
zxwtry <zxwtry@qq.com>
2016-05-17 20:01:36 +0800

    Modify aFunction01.py

--- aproject/aFunction01.py
+++ aproject/aFunction01.py
@@ -6,6 +6,31 @@
 #_*_ coding:utf8 _*_
 
 
+def fun06(name="defName", age=0):
+    print "name : %s " %name
+    print "age : %d " %age
+
+
+d = {'age':30, 'name':"Jack"}
+fun06(**d)
+d['age'] = 31
+fun06(**d)
+
+d = {'age':30, 'name':"Jack",'val':"100"}
+fun06(**d)
+fun06(age=30, name="millo")
+
+fun06()
+fun06("myName", 100)
+
+def fun05(x,y):
+    print "%s : %s" %(x, y)
+
+fun05('name1', 'name2') 
+
+t = ('name3', 'name4')
+fun05(*t)
+
 def fun04(x=1,y=1):
     print "create ",x, "yuan", y,"ge", "icecream"
 
