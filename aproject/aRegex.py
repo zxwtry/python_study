45e47b9af614553348d2cbf4754c46fa488ff47b
zxwtry <zxwtry@qq.com>
2016-05-24 22:55:27 +0800

    heheda

--- aproject/aRegex.py
+++ aproject/aRegex.py
@@ -4,3 +4,22 @@
 #. ^ $ * + ? {} [] \ | ()
 #大部分字母和字符一般都会和自身匹配
 #
+
+import re
+s=r'abc'
+print re.findall(s,"ababc")
+st = "top tip tqp tcp tep tvp"
+s=r"top"
+print re.findall(s,st)
+s=r"t[oi]p"
+print re.findall(s,st)
+s=r"t[^oi]p"
+print re.findall(s,st)
+s=r"hello"
+print re.findall(s,"bbhelloaahellopp")
+s=r"^hello"
+print re.findall(s,"bbhelloaahellopp")
+s=r"^bbhello"
+print re.findall(s,"bbhelloaahellopp")
+s=r"opp$"
+print re.findall(s,"bbhelloaahellopp")
