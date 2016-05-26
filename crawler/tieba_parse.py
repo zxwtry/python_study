418f44c14407d7bab65aeebd2a77eee0a3c90566
zxwtry <zxwtry@qq.com>
2016-05-26 21:31:22 +0800

    heheda

--- crawler/tieba_parse.py
+++ crawler/tieba_parse.py
@@ -6,11 +6,14 @@ html_read=open("tieba_content.txt")
 html_str=html_read.readline()
 html_read.close()
 l20=range(20)
-index=[]
+index=list()
+end=int(20)
 for l in l20:
-     index.append(html_str.find(str(l+1)+".&#160;"));
-
-print index[4]
+    index.append(html_str.find(str(l+1)+".&#160;"));
+    if index[l] == -1:
+        end = l+1;
+print end
+print index[19]
 #tieba_content=open('tieba_content.txt')
 #st=tieba_content.readlines();
 #print st
