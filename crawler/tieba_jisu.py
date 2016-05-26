1e8195d678bbfa02f8dbf0e285c03cb76b2c84a3
zxwtry <zxwtry@qq.com>
2016-05-27 02:35:11 +0800

    kekeda

--- crawler/tieba_jisu.py
+++ crawler/tieba_jisu.py
@@ -7,11 +7,8 @@ import httplib2
 base_url = 'http://tieba.baidu.com/mo/q---1973E7F76569BE76AADDCE7E1592E79C%3AFG%3D1-sz%40320_240%2C-1-3-0--2--wapp_1453584673349_887/m?tn=bdIndex&lp=5014'
 
 #这里是得到某个贴吧的url，如果吧不存在，“xxx吧”尚未建立
-ba_url_0="http://tieba.baidu.com/mo/q---1973E7F76569BE76AADDCE7E1592E79C%3AFG%3D1-sz%40320_240%2C-1-3-0--2--wapp_1453584673349_887/m?kw="
-#ba_url_1="i&lp=1041"
-ba_url_1="&lp=6024"
 def getBaURL(ba_name):
-    return ba_url_0+ba_name+ba_url_1
+    return "http://tieba.baidu.com/mo/q---1973E7F76569BE76AADDCE7E1592E79C%3AFG%3D1-sz%40320_240%2C-1-3-0--2--wapp_1453584673349_887/m?kw="+ba_name+"&lp=6024"
 
 #现在暂时不考虑cookie，通过url，可以得到(resp, content)
 def visitURL(url):
@@ -33,20 +30,23 @@ def getTie20(content):
         else:
             new_end=content.find("</a>",new_index)
             add=8 if l<9 else 9
-            tie20.append(content[new_index+add:new_end].replace("&#160;"," "))
+            tie20.append(content[new_index+add:new_end].replace("&#160;"," ").replace("&quot;"," "))
             ref_start=content.find("<a href=\"m?kz=",new_index-80)
             ref_end=content.find("&",ref_start)
-            ref20.append(content[ref_start:ref_end])
-    l20=range(tie20_end)
-    for l in l20:
-        print tie20[l]
-    for l in l20:
-        print ref20[l]
+            ref20.append(content[ref_start+14:ref_end])
+    return {"tie20":tie20, "ref20":ref20, "tie20_end":tie20_end}
+
+#进入帖子，看楼
+def getTieURL(tiekz):
+    return "http://tieba.baidu.com/mo/q---1973E7F76569BE76AADDCE7E1592E79C%3AFG%3D1-sz%40320_240%2C-1-3-0--2--wapp_1453584673349_887/m?kz="+tiekz+"&is_bakan=0&lp=5010&pinf=1_2_0"
 
 #测试
 name=raw_input("输入需要访问的贴吧名：")
 url_all=visitURL(getBaURL(name))
-getTie20(url_all.get("content"))
+tie=getTie20(url_all.get("content"))
+tie20=tie.get("tie20")
+for l in range(int(tie.get("tie20_end"))):
+    print tie20[l]
 
 
 
