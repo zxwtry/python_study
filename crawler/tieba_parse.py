87ea0588963cd4cb64e15f95c5f2a9be9c96680c
zxwtry <zxwtry@qq.com>
2016-05-26 21:19:40 +0800

    heheda

--- crawler/tieba_parse.py
+++ crawler/tieba_parse.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 #encoding=utf-8
-tieba_content=open('tieba_content.txt')
-st=tieba_content.readlines();
-print st
+from HTMLParser import HTMLParser
+     
+html_read=open("tieba_content.txt")
+html_str=html_read.readline()
+html_read.close()
+l20=range(20)
+index=[]
+for l in l20:
+     index.append(html_str.find(str(l+1)+".&#160;"));
+
+print index[4]
+#tieba_content=open('tieba_content.txt')
+#st=tieba_content.readlines();
+#print st
